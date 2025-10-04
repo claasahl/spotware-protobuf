0c7c444728b5a88d846ca352478c00de84c2f193
Claas Ahlrichs <claasahl@gmail.com>
2025-10-04 21:56:09 +0200

    generate Messages type

--- src/compile/models/generateInterfacesIndex.ts
+++ src/compile/models/generateInterfacesIndex.ts
@@ -4,6 +4,15 @@ export function generateInterfacesIndex(schema: Schema): string {
   const exports = schema.messages.map((message) => {
     return `export * from "./${message.name}.js";`;
   });
+  const imports = schema.messages.map((message) => {
+    return `import { ${message.name}} from "./${message.name}.js";`;
+  });
+  const messages = schema.messages.map((m) => m.name);
   return `export * from "./BaseMessage.js";
-  ${exports.join("\n")}`;
+  ${exports.join("\n")}
+  
+  ${imports.join("\n")}
+
+  export type Messages = ${messages.join(" | ")};
+  `;
 }
--- src/models/interfaces/index.ts
+++ src/models/interfaces/index.ts
@@ -120,3 +120,248 @@ export * from "./ProtoOAHoliday.js";
 export * from "./ProtoOADynamicLeverage.js";
 export * from "./ProtoOADynamicLeverageTier.js";
 export * from "./ProtoOAPositionUnrealizedPnL.js";
+
+import { ProtoOAApplicationAuthReq } from "./ProtoOAApplicationAuthReq.js";
+import { ProtoOAApplicationAuthRes } from "./ProtoOAApplicationAuthRes.js";
+import { ProtoOAAccountAuthReq } from "./ProtoOAAccountAuthReq.js";
+import { ProtoOAAccountAuthRes } from "./ProtoOAAccountAuthRes.js";
+import { ProtoOAErrorRes } from "./ProtoOAErrorRes.js";
+import { ProtoOAClientDisconnectEvent } from "./ProtoOAClientDisconnectEvent.js";
+import { ProtoOAAccountsTokenInvalidatedEvent } from "./ProtoOAAccountsTokenInvalidatedEvent.js";
+import { ProtoOAVersionReq } from "./ProtoOAVersionReq.js";
+import { ProtoOAVersionRes } from "./ProtoOAVersionRes.js";
+import { ProtoOANewOrderReq } from "./ProtoOANewOrderReq.js";
+import { ProtoOAExecutionEvent } from "./ProtoOAExecutionEvent.js";
+import { ProtoOACancelOrderReq } from "./ProtoOACancelOrderReq.js";
+import { ProtoOAAmendOrderReq } from "./ProtoOAAmendOrderReq.js";
+import { ProtoOAAmendPositionSLTPReq } from "./ProtoOAAmendPositionSLTPReq.js";
+import { ProtoOAClosePositionReq } from "./ProtoOAClosePositionReq.js";
+import { ProtoOATrailingSLChangedEvent } from "./ProtoOATrailingSLChangedEvent.js";
+import { ProtoOAAssetListReq } from "./ProtoOAAssetListReq.js";
+import { ProtoOAAssetListRes } from "./ProtoOAAssetListRes.js";
+import { ProtoOASymbolsListReq } from "./ProtoOASymbolsListReq.js";
+import { ProtoOASymbolsListRes } from "./ProtoOASymbolsListRes.js";
+import { ProtoOASymbolByIdReq } from "./ProtoOASymbolByIdReq.js";
+import { ProtoOASymbolByIdRes } from "./ProtoOASymbolByIdRes.js";
+import { ProtoOASymbolsForConversionReq } from "./ProtoOASymbolsForConversionReq.js";
+import { ProtoOASymbolsForConversionRes } from "./ProtoOASymbolsForConversionRes.js";
+import { ProtoOASymbolChangedEvent } from "./ProtoOASymbolChangedEvent.js";
+import { ProtoOAAssetClassListReq } from "./ProtoOAAssetClassListReq.js";
+import { ProtoOAAssetClassListRes } from "./ProtoOAAssetClassListRes.js";
+import { ProtoOATraderReq } from "./ProtoOATraderReq.js";
+import { ProtoOATraderRes } from "./ProtoOATraderRes.js";
+import { ProtoOATraderUpdatedEvent } from "./ProtoOATraderUpdatedEvent.js";
+import { ProtoOAReconcileReq } from "./ProtoOAReconcileReq.js";
+import { ProtoOAReconcileRes } from "./ProtoOAReconcileRes.js";
+import { ProtoOAOrderErrorEvent } from "./ProtoOAOrderErrorEvent.js";
+import { ProtoOADealListReq } from "./ProtoOADealListReq.js";
+import { ProtoOADealListRes } from "./ProtoOADealListRes.js";
+import { ProtoOAOrderListReq } from "./ProtoOAOrderListReq.js";
+import { ProtoOAOrderListRes } from "./ProtoOAOrderListRes.js";
+import { ProtoOAExpectedMarginReq } from "./ProtoOAExpectedMarginReq.js";
+import { ProtoOAExpectedMarginRes } from "./ProtoOAExpectedMarginRes.js";
+import { ProtoOAMarginChangedEvent } from "./ProtoOAMarginChangedEvent.js";
+import { ProtoOACashFlowHistoryListReq } from "./ProtoOACashFlowHistoryListReq.js";
+import { ProtoOACashFlowHistoryListRes } from "./ProtoOACashFlowHistoryListRes.js";
+import { ProtoOAGetAccountListByAccessTokenReq } from "./ProtoOAGetAccountListByAccessTokenReq.js";
+import { ProtoOAGetAccountListByAccessTokenRes } from "./ProtoOAGetAccountListByAccessTokenRes.js";
+import { ProtoOARefreshTokenReq } from "./ProtoOARefreshTokenReq.js";
+import { ProtoOARefreshTokenRes } from "./ProtoOARefreshTokenRes.js";
+import { ProtoOASubscribeSpotsReq } from "./ProtoOASubscribeSpotsReq.js";
+import { ProtoOASubscribeSpotsRes } from "./ProtoOASubscribeSpotsRes.js";
+import { ProtoOAUnsubscribeSpotsReq } from "./ProtoOAUnsubscribeSpotsReq.js";
+import { ProtoOAUnsubscribeSpotsRes } from "./ProtoOAUnsubscribeSpotsRes.js";
+import { ProtoOASpotEvent } from "./ProtoOASpotEvent.js";
+import { ProtoOASubscribeLiveTrendbarReq } from "./ProtoOASubscribeLiveTrendbarReq.js";
+import { ProtoOASubscribeLiveTrendbarRes } from "./ProtoOASubscribeLiveTrendbarRes.js";
+import { ProtoOAUnsubscribeLiveTrendbarReq } from "./ProtoOAUnsubscribeLiveTrendbarReq.js";
+import { ProtoOAUnsubscribeLiveTrendbarRes } from "./ProtoOAUnsubscribeLiveTrendbarRes.js";
+import { ProtoOAGetTrendbarsReq } from "./ProtoOAGetTrendbarsReq.js";
+import { ProtoOAGetTrendbarsRes } from "./ProtoOAGetTrendbarsRes.js";
+import { ProtoOAGetTickDataReq } from "./ProtoOAGetTickDataReq.js";
+import { ProtoOAGetTickDataRes } from "./ProtoOAGetTickDataRes.js";
+import { ProtoOAGetCtidProfileByTokenReq } from "./ProtoOAGetCtidProfileByTokenReq.js";
+import { ProtoOAGetCtidProfileByTokenRes } from "./ProtoOAGetCtidProfileByTokenRes.js";
+import { ProtoOADepthEvent } from "./ProtoOADepthEvent.js";
+import { ProtoOASubscribeDepthQuotesReq } from "./ProtoOASubscribeDepthQuotesReq.js";
+import { ProtoOASubscribeDepthQuotesRes } from "./ProtoOASubscribeDepthQuotesRes.js";
+import { ProtoOAUnsubscribeDepthQuotesReq } from "./ProtoOAUnsubscribeDepthQuotesReq.js";
+import { ProtoOAUnsubscribeDepthQuotesRes } from "./ProtoOAUnsubscribeDepthQuotesRes.js";
+import { ProtoOASymbolCategoryListReq } from "./ProtoOASymbolCategoryListReq.js";
+import { ProtoOASymbolCategoryListRes } from "./ProtoOASymbolCategoryListRes.js";
+import { ProtoOAAccountLogoutReq } from "./ProtoOAAccountLogoutReq.js";
+import { ProtoOAAccountLogoutRes } from "./ProtoOAAccountLogoutRes.js";
+import { ProtoOAAccountDisconnectEvent } from "./ProtoOAAccountDisconnectEvent.js";
+import { ProtoOAMarginCallListReq } from "./ProtoOAMarginCallListReq.js";
+import { ProtoOAMarginCallListRes } from "./ProtoOAMarginCallListRes.js";
+import { ProtoOAMarginCallUpdateReq } from "./ProtoOAMarginCallUpdateReq.js";
+import { ProtoOAMarginCallUpdateRes } from "./ProtoOAMarginCallUpdateRes.js";
+import { ProtoOAMarginCallUpdateEvent } from "./ProtoOAMarginCallUpdateEvent.js";
+import { ProtoOAMarginCallTriggerEvent } from "./ProtoOAMarginCallTriggerEvent.js";
+import { ProtoOAGetDynamicLeverageByIDReq } from "./ProtoOAGetDynamicLeverageByIDReq.js";
+import { ProtoOAGetDynamicLeverageByIDRes } from "./ProtoOAGetDynamicLeverageByIDRes.js";
+import { ProtoOADealListByPositionIdReq } from "./ProtoOADealListByPositionIdReq.js";
+import { ProtoOADealListByPositionIdRes } from "./ProtoOADealListByPositionIdRes.js";
+import { ProtoOAOrderDetailsReq } from "./ProtoOAOrderDetailsReq.js";
+import { ProtoOAOrderDetailsRes } from "./ProtoOAOrderDetailsRes.js";
+import { ProtoOAOrderListByPositionIdReq } from "./ProtoOAOrderListByPositionIdReq.js";
+import { ProtoOAOrderListByPositionIdRes } from "./ProtoOAOrderListByPositionIdRes.js";
+import { ProtoOADealOffsetListReq } from "./ProtoOADealOffsetListReq.js";
+import { ProtoOADealOffsetListRes } from "./ProtoOADealOffsetListRes.js";
+import { ProtoOAGetPositionUnrealizedPnLReq } from "./ProtoOAGetPositionUnrealizedPnLReq.js";
+import { ProtoOAGetPositionUnrealizedPnLRes } from "./ProtoOAGetPositionUnrealizedPnLRes.js";
+import { ProtoOAv1PnLChangeEvent } from "./ProtoOAv1PnLChangeEvent.js";
+import { ProtoOAv1PnLChangeSubscribeReq } from "./ProtoOAv1PnLChangeSubscribeReq.js";
+import { ProtoOAv1PnLChangeSubscribeRes } from "./ProtoOAv1PnLChangeSubscribeRes.js";
+import { ProtoOAv1PnLChangeUnSubscribeReq } from "./ProtoOAv1PnLChangeUnSubscribeReq.js";
+import { ProtoOAv1PnLChangeUnSubscribeRes } from "./ProtoOAv1PnLChangeUnSubscribeRes.js";
+import { ProtoOAAsset } from "./ProtoOAAsset.js";
+import { ProtoOASymbol } from "./ProtoOASymbol.js";
+import { ProtoOALightSymbol } from "./ProtoOALightSymbol.js";
+import { ProtoOAArchivedSymbol } from "./ProtoOAArchivedSymbol.js";
+import { ProtoOASymbolCategory } from "./ProtoOASymbolCategory.js";
+import { ProtoOAInterval } from "./ProtoOAInterval.js";
+import { ProtoOATrader } from "./ProtoOATrader.js";
+import { ProtoOAPosition } from "./ProtoOAPosition.js";
+import { ProtoOATradeData } from "./ProtoOATradeData.js";
+import { ProtoOAOrder } from "./ProtoOAOrder.js";
+import { ProtoOABonusDepositWithdraw } from "./ProtoOABonusDepositWithdraw.js";
+import { ProtoOADepositWithdraw } from "./ProtoOADepositWithdraw.js";
+import { ProtoOADeal } from "./ProtoOADeal.js";
+import { ProtoOADealOffset } from "./ProtoOADealOffset.js";
+import { ProtoOAClosePositionDetail } from "./ProtoOAClosePositionDetail.js";
+import { ProtoOATrendbar } from "./ProtoOATrendbar.js";
+import { ProtoOAExpectedMargin } from "./ProtoOAExpectedMargin.js";
+import { ProtoOATickData } from "./ProtoOATickData.js";
+import { ProtoOACtidProfile } from "./ProtoOACtidProfile.js";
+import { ProtoOACtidTraderAccount } from "./ProtoOACtidTraderAccount.js";
+import { ProtoOAAssetClass } from "./ProtoOAAssetClass.js";
+import { ProtoOADepthQuote } from "./ProtoOADepthQuote.js";
+import { ProtoOAMarginCall } from "./ProtoOAMarginCall.js";
+import { ProtoOAHoliday } from "./ProtoOAHoliday.js";
+import { ProtoOADynamicLeverage } from "./ProtoOADynamicLeverage.js";
+import { ProtoOADynamicLeverageTier } from "./ProtoOADynamicLeverageTier.js";
+import { ProtoOAPositionUnrealizedPnL } from "./ProtoOAPositionUnrealizedPnL.js";
+
+export type Messages =
+  | ProtoOAApplicationAuthReq
+  | ProtoOAApplicationAuthRes
+  | ProtoOAAccountAuthReq
+  | ProtoOAAccountAuthRes
+  | ProtoOAErrorRes
+  | ProtoOAClientDisconnectEvent
+  | ProtoOAAccountsTokenInvalidatedEvent
+  | ProtoOAVersionReq
+  | ProtoOAVersionRes
+  | ProtoOANewOrderReq
+  | ProtoOAExecutionEvent
+  | ProtoOACancelOrderReq
+  | ProtoOAAmendOrderReq
+  | ProtoOAAmendPositionSLTPReq
+  | ProtoOAClosePositionReq
+  | ProtoOATrailingSLChangedEvent
+  | ProtoOAAssetListReq
+  | ProtoOAAssetListRes
+  | ProtoOASymbolsListReq
+  | ProtoOASymbolsListRes
+  | ProtoOASymbolByIdReq
+  | ProtoOASymbolByIdRes
+  | ProtoOASymbolsForConversionReq
+  | ProtoOASymbolsForConversionRes
+  | ProtoOASymbolChangedEvent
+  | ProtoOAAssetClassListReq
+  | ProtoOAAssetClassListRes
+  | ProtoOATraderReq
+  | ProtoOATraderRes
+  | ProtoOATraderUpdatedEvent
+  | ProtoOAReconcileReq
+  | ProtoOAReconcileRes
+  | ProtoOAOrderErrorEvent
+  | ProtoOADealListReq
+  | ProtoOADealListRes
+  | ProtoOAOrderListReq
+  | ProtoOAOrderListRes
+  | ProtoOAExpectedMarginReq
+  | ProtoOAExpectedMarginRes
+  | ProtoOAMarginChangedEvent
+  | ProtoOACashFlowHistoryListReq
+  | ProtoOACashFlowHistoryListRes
+  | ProtoOAGetAccountListByAccessTokenReq
+  | ProtoOAGetAccountListByAccessTokenRes
+  | ProtoOARefreshTokenReq
+  | ProtoOARefreshTokenRes
+  | ProtoOASubscribeSpotsReq
+  | ProtoOASubscribeSpotsRes
+  | ProtoOAUnsubscribeSpotsReq
+  | ProtoOAUnsubscribeSpotsRes
+  | ProtoOASpotEvent
+  | ProtoOASubscribeLiveTrendbarReq
+  | ProtoOASubscribeLiveTrendbarRes
+  | ProtoOAUnsubscribeLiveTrendbarReq
+  | ProtoOAUnsubscribeLiveTrendbarRes
+  | ProtoOAGetTrendbarsReq
+  | ProtoOAGetTrendbarsRes
+  | ProtoOAGetTickDataReq
+  | ProtoOAGetTickDataRes
+  | ProtoOAGetCtidProfileByTokenReq
+  | ProtoOAGetCtidProfileByTokenRes
+  | ProtoOADepthEvent
+  | ProtoOASubscribeDepthQuotesReq
+  | ProtoOASubscribeDepthQuotesRes
+  | ProtoOAUnsubscribeDepthQuotesReq
+  | ProtoOAUnsubscribeDepthQuotesRes
+  | ProtoOASymbolCategoryListReq
+  | ProtoOASymbolCategoryListRes
+  | ProtoOAAccountLogoutReq
+  | ProtoOAAccountLogoutRes
+  | ProtoOAAccountDisconnectEvent
+  | ProtoOAMarginCallListReq
+  | ProtoOAMarginCallListRes
+  | ProtoOAMarginCallUpdateReq
+  | ProtoOAMarginCallUpdateRes
+  | ProtoOAMarginCallUpdateEvent
+  | ProtoOAMarginCallTriggerEvent
+  | ProtoOAGetDynamicLeverageByIDReq
+  | ProtoOAGetDynamicLeverageByIDRes
+  | ProtoOADealListByPositionIdReq
+  | ProtoOADealListByPositionIdRes
+  | ProtoOAOrderDetailsReq
+  | ProtoOAOrderDetailsRes
+  | ProtoOAOrderListByPositionIdReq
+  | ProtoOAOrderListByPositionIdRes
+  | ProtoOADealOffsetListReq
+  | ProtoOADealOffsetListRes
+  | ProtoOAGetPositionUnrealizedPnLReq
+  | ProtoOAGetPositionUnrealizedPnLRes
+  | ProtoOAv1PnLChangeEvent
+  | ProtoOAv1PnLChangeSubscribeReq
+  | ProtoOAv1PnLChangeSubscribeRes
+  | ProtoOAv1PnLChangeUnSubscribeReq
+  | ProtoOAv1PnLChangeUnSubscribeRes
+  | ProtoOAAsset
+  | ProtoOASymbol
+  | ProtoOALightSymbol
+  | ProtoOAArchivedSymbol
+  | ProtoOASymbolCategory
+  | ProtoOAInterval
+  | ProtoOATrader
+  | ProtoOAPosition
+  | ProtoOATradeData
+  | ProtoOAOrder
+  | ProtoOABonusDepositWithdraw
+  | ProtoOADepositWithdraw
+  | ProtoOADeal
+  | ProtoOADealOffset
+  | ProtoOAClosePositionDetail
+  | ProtoOATrendbar
+  | ProtoOAExpectedMargin
+  | ProtoOATickData
+  | ProtoOACtidProfile
+  | ProtoOACtidTraderAccount
+  | ProtoOAAssetClass
+  | ProtoOADepthQuote
+  | ProtoOAMarginCall
+  | ProtoOAHoliday
+  | ProtoOADynamicLeverage
+  | ProtoOADynamicLeverageTier
+  | ProtoOAPositionUnrealizedPnL;
